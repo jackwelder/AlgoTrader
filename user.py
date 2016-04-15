4621b800c3afa9d4a602e8855d1d550a5d401539
Nick Fiacco <nicholas.r.fiacco.17@dartmouth.edu>
2016-04-14 20:18:48 -0400

    Added python script to get x number of previous tweets from user

---
+++ user.py
@@ -0,0 +1,75 @@
+# title: user.py
+# author: nick fiacco
+#
+# description:
+# this program allows you to run a query on all of a user's previous
+# tweets, given their StockTwits user name.
+#
+# dependencies:
+# StockTwits API access
+# requests
+# json
+#
+# example usage: python user.py TrustedBinary
+
+from __future__ import print_function
+import requests
+import json
+import sys
+
+# username from which we will fetch tweets
+user = sys.argv[1]
+
+# number of tweets to fetch
+count = int(sys.argv[2])
+
+# output file
+f = open('output.txt','w')
+
+# initial api request url
+url = "https://api.stocktwits.com/api/2/streams/user/" + user + ".json"
+
+response = requests.get(url)
+
+json_data = response.json()
+
+oldest_id = 0
+
+for tweet in json_data['messages']:
+	print(tweet['body'], file = f)
+	print("", file = f)
+	oldest_id = int(tweet['id'])
+
+
+while True:
+	# generate new urls for older tweets using tweet ids
+	new_url  = "https://api.stocktwits.com/api/2/streams/user/" + user + ".json?max=" + str(oldest_id)
+
+	response = requests.get(new_url)
+
+	if response == "<Response [500]>":
+		print("No more tweets\n")
+		break
+
+	if count <= 0:
+		print("Reached maximum tweet amount\n")
+		break
+
+	json_data = response.json()
+
+	for tweet in json_data['messages']:
+		print(tweet['body'], file = f)
+		print("", file = f)
+		oldest_id = int(tweet['id'])
+
+	count -= 30
+
+f.close()
+
+
+
+
+
+
+
+
